5efc6bcd17fb02fc0378cd8f8e5e66cc2e05d789
Sebastiaan Stoffels <seb@sebastiaan-stoffels.com>
2017-11-28 20:05:33 +1100

    removed un-needed return value in download_file()

--- bulk-insert.py
+++ bulk-insert.py
@@ -1,9 +1,10 @@
 import psycopg2
+import time
 
 # variables to hold info for what we want to import
-table_name = 'AUDJPY_ticks'
+table_name = 'AUDUSD2_ticks'
 Database = 'dbname=truefxdb user=sebastiaan'
-csv_file = '/home/seb-i3/truefx/AUDJPY-2017-01.csv'
+csv_file = '/home/sebastiaan/truefx/AUDUSD-2017-10.csv'
 command = "CREATE TABLE " + table_name + " (currency text, datetime timestamp, bid numeric, ask numeric)"
 
 # connect to the DB
@@ -11,11 +12,24 @@ conn = psycopg2.connect(Database)
 cur = conn.cursor()
 
 # create the table
-cur.execute(command)
-conn.commit()
+try:
+    cur.execute(command)
+    conn.commit()
+except psycopg2.ProgrammingError:
+    print("table exists")
+    conn.commit()
 
+cur = conn.cursor()
+start = time.time()
 # import the contents of 'csv_file' into the table
 with open(csv_file, 'r') as import_file:
+    row_count  = 0
+    for lines in import_file:
+        row_count += 1
+    import_file.seek(0)
     cur.copy_from(import_file, table_name, ',')
+
 conn.commit()
 conn.close()
+elapsed = time.time() - start
+print("imported {} rows in: {:.2f} seconds".format(row_count, elapsed))
