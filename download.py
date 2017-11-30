bbcb1e4cc55152822526af2bb1f4522eb12283b5
Sebastiaan Stoffels <seb@sebastiaan-stoffels.com>
2017-11-30 16:56:11 +1100

    comments changed to Docstrings, parameterization of download username/pass and save path.

--- download.py
+++ download.py
@@ -2,10 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 from clint.textui import progress
 
-# this function downloads the files - change the path below to your preferred full path
-# make sure you have permission to write to the directory you choose
-def download_file(url):
-    local_filename = '/home/seb-i3/truefx/' + url.split('/')[-1]
+def download_file(url, download_path):
+    """this function downloads the files - change the path below to your preferred full path
+    make sure you have permission to write to the directory you choose"""
+    #local_filename = '/home/seb-i3/truefx/' + url.split('/')[-1]
+    local_filename = download_path + url.split('/')[-1]
     print('downloading: {}'.format(url))
     r = requests.get(url, stream=True)
 
@@ -17,34 +18,32 @@ def download_file(url):
                 f.flush()
     return
 
-# this function traverses the truefx download folders one by one until we find the zip files we want
-# once it finds them - we call the download_file() function
-def traverse(html, session):
+def traverse(html, session, download_path):
+    """this function traverses the truefx download folders one by one until we find the zip files we want
+    once it finds them - we call the download_file() function"""
     for link in html.find_all('a'):
         if 'color:#CCC;' not in str(link.get_attribute_list('style')) and 'folderbg' in str(link.get_attribute_list('class')):
-            next_level = 'https://www.truefx.com/' + str(link.get("href"))
+            next_level = 'https://www.truefx.com/{}'.format(str(link.get("href")))
             response_next = session.get(next_level)
             next_html = BeautifulSoup(response_next.content, 'html.parser')
 
             # recursive call
-            traverse(next_html, session)
+            traverse(next_html, session, download_path)
 
         elif 'dl-zip' in str(link.get_attribute_list('class')):
-            download_file('https://www.truefx.com' + str(link.get('href')))
+            download_file('https://www.truefx.com' + str(link.get('href')), download_path)
     return
 
-# this function establishes the session and parses the main download page. We then call traverse()
-def find_files(url):
-    # Put your truefx username and password in place of <username> and <password> leave the ' ' quotes
-    payload = {'USERNAME': '<username>', 'PASSWORD': '<password>'}
-
+def find_files(username, password, download_path):
+    """this function establishes the session and parses the main download page. We then call traverse()"""
+    payload = {'USERNAME': '{}'.format(username), 'PASSWORD': '{}'.format(password)}
+    url = 'https://www.truefx.com/?page=downloads'
     with requests.Session() as session:
         session.post('https://www.truefx.com/?page=loginz', data=payload)
         response = session.get(url)
         html = BeautifulSoup(response.content, 'html.parser')
-        traverse(html, session)
+        traverse(html, session, download_path)
     return
 
-
-# Call the find_files function to start things off
-find_files('https://www.truefx.com/?page=downloads')
+if __name__ == "__main__":
+    find_files('YOUR-USER-NAME', 'YOUR-PASSWORD', '/your/path/')
