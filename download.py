2eaeaff74994cd9844036fd97db87c630946ff34
Sebastiaan Stoffels <seb@sebastiaan-stoffels.com>
2019-02-10 09:21:01 +1100

    added '2019' to years list

--- download.py
+++ download.py
@@ -8,7 +8,7 @@ import os
 
 currencies = ['AUDJPY', 'AUDNZD', 'AUDUSD', 'CADJPY', 'CHFJPY', 'EURCHF', 'EURGBP', 'EURJPY', 'EURUSD', 'GBPJPY', 'GBPUSD', 'NZDUSD', 'USDCAD', 'USDCHF', 'USDJPY']
 months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
-years = ['2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018']
+years = ['2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019']
 
 
 def download_file(url, download_path):
